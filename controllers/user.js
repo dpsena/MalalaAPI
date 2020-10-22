591a722c05ce3f37ada3047830a1e6378b0d91f0
dpsena <yeceniagmoreno2@gmail.com>
2020-10-21 19:05:34 -0500

    user modelo-ruta-controlador

---
+++ controllers/user.js
@@ -0,0 +1,52 @@
+const user = require('../models/user')
+const UserModel = require('../models/user')
+exports.create = (req, res) => {
+
+    if (Object.entries(req.body).length==0) {
+        return res.status(400).send({
+            message: 'los datos  del usuario son obligatorios.'
+        })
+    }
+    const user = new UserModel({
+        name: req.body.name,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role,
+        identificationNumber: req.body.identificationNumber,
+       
+    })
+    user.save()
+        .then((dataUser) => {
+            res.send(dataUser)
+        })
+        .catch((error) => {
+            res.status(500).send({
+                message: error.message
+            })
+        })
+}
+
+exports.update=(req,res)=>{
+    if (Object.entries(req.body).length==0) {
+        return res.status(400).send({
+            message: 'los datos a actualizar deben estar llenos.'
+        })
+    }
+    const user={
+        name: req.body.name,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        
+    }
+    UserModel.findByIdAndUpdate(req.params.id,user)
+    .then((userUpdate) => {
+        res.send(userUpdate)
+    })
+    .catch((error) => {
+        res.status(500).send({
+            message: error.message
+        })
+    })
+}
