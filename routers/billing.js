892b37f81f984b941fbc7513c2bbf26a9d56727d
dpsena <yeceniagmoreno2@gmail.com>
2020-10-27 17:58:28 -0500

    se realizan cambios en facturacion y ataraxia delete y get

--- routers/billing.js
+++ routers/billing.js
@@ -2,4 +2,7 @@ module.exports=(app)=>{
     const billing=require('../controllers/billing')
     app.post('/billing/create',billing.create)
     app.put('/billing/update/:id',billing.update)
+    app.get('/billing/getAll',billing.getAll)
+    app.get('/billing/getOne/:id',billing.getOne)
+    app.delete('/billing/delete/:id',billing.deleteOne)
 }
