39ba28fde18c36d10cfddc95ce309d8130fd22a6
IvetteCastelblanco <ivetht200130@gmail.com>
2020-10-30 15:55:38 -0500

    Se crea conexion a heroku

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const config = require ('./config')
+
     const conectBD = () => {
-        mongoose.connect('mongodb+srv://Ataraxia:ataraxia@ataraxia.usy4w.mongodb.net/Ataraxia?retryWrites=true&w=majority', {
+        mongoose.connect( config.mongoDB, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
             },(error) => {
