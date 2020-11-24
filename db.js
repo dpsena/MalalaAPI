c000bcbb883f3792d3cafb9711e4dadc49e1628c
lunagomez26 <luvagoba@gmail.com>
2020-11-24 14:30:51 -0500

    Se areglaron los problemas de conexion

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const config = require ('./config')
 
     const conectBD = () => {
 
-        mongoose.connect(config.mongoDB, {
+        mongoose.connect( config.mongoDB,{
                 useNewUrlParser: true,
                 useUnifiedTopology: true
             },(error) => {
